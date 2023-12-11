cb6aeae6114fdadfc3f4846a757e15271b53c5e8
Ridlo Achmad Ghifary <ridloghfry@gmail.com>
2023-12-11 20:44:26 +0700

    create responsive /Pricing page

---
+++ src/app/pricing/page.jsx
@@ -0,0 +1,11 @@
+import PageBackground from "../components/pageBackground"
+import Pricing from "../homePage/pricing/pricing"
+
+export default function PricingPage() {
+  return (
+    <section>
+      <PageBackground name="Pricing" />
+      <Pricing />
+    </section>
+  )
+}
